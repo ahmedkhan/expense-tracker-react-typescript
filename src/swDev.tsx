4dc5cbd583cfd2a65a498b65a43ae96da8e4885b
Ahmed Khan <ahmed.khan8845@gmail.com>
2020-08-25 18:42:36 +0500

    Update sw.js

--- src/swDev.tsx
+++ src/swDev.tsx
@@ -1,16 +1,15 @@
+
 export default function swDev() {
 
-    // Check for browser support of service worker
-    if ('serviceWorker' in navigator) {
-        navigator.serviceWorker.register('/swDev.js')
-            .then(function (registration) {
-                // Successful registration
-                console.log('Hooray. Registration successful, scope is:', registration.scope);
-            }).catch(function (err) {
-                // Failed registration, service worker won’t be installed
-                console.log('Whoops. Service worker registration failed, error:', err);
-            });
-    }
+  // Check for browser support of service worker
+  if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.register('/swDev.js')
+          .then(function (registration) {
+              // Successful registration
+              console.log('Hooray. Registration successful, scope is:', registration.scope);
+          }).catch(function (err) {
+              // Failed registration, service worker won’t be installed
+              console.log('Whoops. Service worker registration failed, error:', err);
+          });
+  }
 }
-
-
