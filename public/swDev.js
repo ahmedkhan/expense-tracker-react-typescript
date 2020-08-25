6e0ff67142387a37ab097cc213425bd0f33175bb
Ahmed Khan <ahmed.khan8845@gmail.com>
2020-08-26 04:40:49 +0500

    update sw.js

--- public/swDev.js
+++ public/swDev.js
@@ -1,67 +1,27 @@
 
+let cacheName = "AppV1";
 
-var CACHE_NAME = 'my-pwa-cache-v1';
-var urlsToCache = [
-  "/",
-  "/favicon.icon",
-  "/asset-manifest.json",
-  "/manifest.json",
-  "/swDev.js",
-  "/index.html",
-  "/static/js/2.c0adc85b.chunk.js",
-  "/static/js/main.cd07c24b.chunk.js",
-  "/static/js/runtime-main.ff41a76f.js",
-  "/static/css/main.91bf5878.chunk.css"
-];
+this.addEventListener("install", (event)=>{
+ event.waitUntil(
+   caches.open(cacheName).then((cache)=>{
+     cache.addAll([
+       "/static/js/bundle.js",
+       "/static/js/1.chunk.js",
+       "/static/js/main.chunk.js",
+       "/index.html",
+       "/" 
+     ])
+   })
+ )
+})
 
-this.addEventListener('install', function (event) {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(caches => {
-        return caches.addAll(urlsToCache);
-      })
-  );
-});
 
-this.addEventListener('activate',function(event){
-  event.waitUntil(
-    caches.keys().then(function(cacheName){
-      return Promise.all(
-        cacheName.filter(function(cacheName){
-        }).map(function(cacheName){
-          return caches.delete(cacheName)
-        })
-      )
+this.addEventListener("fetch",(event)=>{
+  event.respondWith(
+    caches.match(event.request).then((resp)=>{
+      if(resp){
+        return resp
+      }
     })
   )
 })
-
-this.addEventListener('fetch',event=> {
-   event.respondWith(
-     caches.match(event.request)
-     .then(function(response){
-       if(response){
-         console.log("Found in cache");
-         console.log(event.request);
-         console.log(response);
-         return response;      
-
-       }
-       console.log("Not Found in Cache");
-       console.log(event.request);
-       return fetch(event.request);
-     })
-   )
-})
-  
- 
-
-
-
-
-
-
-
-
-  
-    
