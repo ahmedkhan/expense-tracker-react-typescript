05776bb9d43b26eaa5ebc063385f83387e65ad39
Ahmed Khan <ahmed.khan8845@gmail.com>
2020-09-06 03:11:03 +0500

    update sw

--- public/sw.js
+++ public/sw.js
@@ -1,9 +1,9 @@
-var CACHE_NAME = 'Static'
-
+var CACHE_STATIC_NAME = 'Static-V4'
+var CACHE_DYNAMIC_NAME = 'dynamic-V2'
 var urlsToCache = [
     "/",
     "/static/js/bundle.js",
-    "/static/js/0.chunk.js",
+    "/static/js/1.chunk.js",
     "static/js/main.chunk.js",
     "/sw.js",
     "/favicon.ico",
@@ -15,7 +15,7 @@ var urlsToCache = [
 ]
 this.addEventListener('install', function(event) {
     event.waitUntil(
-        caches.open(CACHE_NAME)
+        caches.open(CACHE_STATIC_NAME)
             .then((cache) => {
                 console.log('Opened Cache')
                 return (
@@ -29,6 +29,18 @@ this.addEventListener('install', function(event) {
 
 this.addEventListener('activate',function(event) {
     console.log("[ServiceWorker] Activaing Service worker...",event)
+    event.waitUntil(
+        caches.keys()
+         .then(function(keyList){
+             return Promise.all(keyList.map((key)=>{
+                 if (key !==CACHE_STATIC_NAME && key !== CACHE_DYNAMIC_NAME){
+                     console.log("Service Worker Removing Old Caches...", key)
+                     return caches.delete(key);
+
+                 }
+             }));
+         })
+    );
     return this.clients.claim();
 });
  
@@ -40,6 +52,16 @@ this.addEventListener('fetch',function(event) {
                 return response
             } else {
                 return fetch(event.request)
+                .then(function(res){
+                    return caches.open(CACHE_DYNAMIC_NAME)
+                     .then(function(cache){
+                         cache.put(event.request.url , res.clone());
+                         return res
+                     })
+                })
+                .catch(function(err){
+
+                });
             }
         })
     );
